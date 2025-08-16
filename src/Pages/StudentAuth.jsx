8689f66cf88e650852fbe4eaf723270e12f4dea5
Shivam Prakash <shivamprakash444@gmail.com>
2025-08-16 21:36:41 +0530

    fix in StudentAuth: Grad yeaar fix

--- src/Pages/StudentAuth.jsx
+++ src/Pages/StudentAuth.jsx
@@ -505,9 +505,9 @@ export default function StudentAuth() {
                           <SelectValue placeholder="Select year" />
                         </SelectTrigger>
                         <SelectContent>
-                          <SelectItem value="2023">2021</SelectItem>
-                          <SelectItem value="2024">2022</SelectItem>
-                          <SelectItem value="2024">2023</SelectItem>
+                          <SelectItem value="2021">2021</SelectItem>
+                          <SelectItem value="2022">2022</SelectItem>
+                          <SelectItem value="2023">2023</SelectItem>
                           <SelectItem value="2024">2024</SelectItem>
                           <SelectItem value="2025">2025</SelectItem>
                           <SelectItem value="2026">2026</SelectItem>
